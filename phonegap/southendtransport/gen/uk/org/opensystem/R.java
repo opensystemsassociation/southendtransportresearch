e44d702bf3468c6db18be7e5b62098f067e19080
Tom Keene <tom@theanthillsocial.co.uk>
2013-03-27 17:47:40 +0000

    Compiled classes

--- phonegap/southendtransport/gen/uk/org/opensystem/R.java
+++ phonegap/southendtransport/gen/uk/org/opensystem/R.java
@@ -15,14 +15,22 @@ public final class R {
         public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int preview=0x7f060000;
+        public static final int preview=0x7f070000;
+        public static final int stop_activity=0x7f070002;
+        public static final int switch_cam=0x7f070001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int surface=0x7f030001;
     }
+    public static final class menu {
+        public static final int camera_menu=0x7f060000;
+    }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int hide_show_cam=0x7f050001;
+        public static final int stop_activity=0x7f050003;
+        public static final int switch_cam=0x7f050002;
     }
     public static final class xml {
         public static final int config=0x7f040000;
