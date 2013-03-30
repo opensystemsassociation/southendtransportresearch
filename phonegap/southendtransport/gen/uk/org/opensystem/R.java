4b7ff531718e3ae560923483b5611e649221e423
Tom Keene <tom@theanthillsocial.co.uk>
2013-03-30 14:21:22 +0000

    Altered manifest to include USB

--- phonegap/southendtransport/gen/uk/org/opensystem/R.java
+++ phonegap/southendtransport/gen/uk/org/opensystem/R.java
@@ -33,6 +33,7 @@ public final class R {
         public static final int switch_cam=0x7f050002;
     }
     public static final class xml {
-        public static final int config=0x7f040000;
+        public static final int accessory_filter=0x7f040000;
+        public static final int config=0x7f040001;
     }
 }
